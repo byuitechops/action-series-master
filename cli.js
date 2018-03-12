df528182bbd922e31bfa509cd47a4e7f043e8cf1
sethchilders92 <sethchilders92@gmail.com>
2018-03-12 13:27:10 -0600

    Broke the gauntlet, but fixed cli.js....almost

--- cli.js
+++ cli.js
@@ -8,6 +8,8 @@ enquirer.ask()
     .then(answers => {
         createCourseObject({}, (err, course) => {
             course.info.checkStandards = true;
+            course.info.canvasOU = answers.canvasID;
+            console.log(`course.info: ${JSON.stringify(course.info, null, '\t')}`);
             main(course, (err, courseObj) => {
                 if (err) console.log(err);
                 else console.log('Check complete');
