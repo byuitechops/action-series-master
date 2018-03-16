f05f362fd7ce2c0611536c11e1763eb2228d2468
Zachary Williams <wil11063@byui.edu>
2018-03-16 16:15:39 -0600

    Async/Await Goodness in CLI

--- cli.js
+++ cli.js
@@ -16,32 +16,51 @@ function getPristine() {
     });
 }
 
-function runActionSeries(foundCourse) {
+function getUserCourse(courseID) {
     return new Promise((resolve, reject) => {
-        /* Register the question with the found gauntlet as the default */
-        enquirer.question('canvasID', 'Canvas Course ID:', { 'default': foundCourse.id });
-
-        /* Ask the user what course to run the action-series on */
-        enquirer.ask()
-            .then(answers => {
-                createCourseObject({}, (err, course) => {
-                    course.info.checkStandards = true;
-                    course.info.canvasOU = answers.canvasID;
-
-                    /* Run the action-series model on the course */
-                    main(course, (err, courseObject) => {
-                        if (err) return reject(err);
-                        resolve(courseObject);
-                    });
-                });
-            });
+        canvas.get(`/api/v1/accounts/1/courses/${courseID}`, (err, givenCourse) => {
+            if (err) return reject(err);
+            resolve(givenCourse);
+        });
     });
 }
 
+function runActionSeries(foundCourse) {
+    return new Promise((resolve, reject) => {
+        createCourseObject({}, (err, course) => {
+            course.info.fileName = foundCourse.course_code;
+            course.info.courseCode = foundCourse.course_code;
+            course.info.courseName = foundCourse.name;
+            course.info.checkStandards = true;
+            course.info.canvasOU = foundCourse.id;
+
+            /* Run the action-series model on the course */
+            main(course, (err, courseObject) => {
+                if (err) return reject(err);
+                resolve(courseObject);
+            });
+        });
+    });
+}
+
+async function promptUser(foundCourse) {
+    /* Register the question with the found gauntlet as the default */
+    enquirer.question('canvasID', 'Canvas Course ID:', { 'default': foundCourse.id });
+
+    /* Ask the user what course to run the action-series on */
+    var answers = await enquirer.ask();
+
+    if (answers.canvasID !== foundCourse.id) {
+        foundCourse = await getUserCourse(answers.canvasID);
+    }
+    return runActionSeries(foundCourse);
+}
+
+
 getPristine()
-    .then(runActionSeries)
+    .then(promptUser)
     .then(courseObject => {
-        console.log('Success');
+        console.log(`${courseObject.info.courseName} completely checked.`);
     })
     .catch(console.error);
 
