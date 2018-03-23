3b89d4c96e7ad6d68daab5a4841c88d941584f57
Sam McGrath <mcg13004@byui.edu>
2018-03-23 09:18:15 -0600

    XML correctly parses

--- actions/universal-description-quicklinks.js
+++ actions/universal-description-quicklinks.js
@@ -1,8 +1,9 @@
 const asyncLib = require('async');
-// const canvas = require('canvas');
+const canvas = require('canvas');
 const cheerio = require('cheerio');
 
 var xmlArray = [];
+var canvasPagesArray = [];
 
 module.exports = (course, item, callback) => {
     if (item.techops.delete === true ||
@@ -11,23 +12,42 @@ module.exports = (course, item, callback) => {
             return;
     } else {
         beginProcess();
-        callback(null, course, item);
     }
 
     function beginProcess() {
-        buildXMLArray((err) => {
-            if (err) {
-                course.error(err);
-                return;
+        checkArrays((checkArraysErr) => {
+            if (checkArraysErr) {
+                course.err(checkArraysErr);
+                callback(null, course, item);
             }
-
-            return;
         });
+    }
+
+    function checkArrays(checkArraysCallback) {
+        if (xmlArray.length < 1) {
+            buildXMLArray((err) => {
+                if (err) {
+                    checkArraysCallback(err);
+                    return;
+                }
+            });
+        }
+
+        if (canvasPagesArray.length < 1) {
+            buildCanvasArray((err) => {
+                if (err) {
+                    checkArraysCallback(err);
+                    return;
+                }
+            });
+        }
+
+        checkArraysCallback(null);
     }
 
     function buildXMLArray(buildXMLArrayCallback) {
         //this is the path for the module in imsmanifest.xml
-        var path = 'manifest > organizations > organization > item';
+        var path = 'manifest > organizations > organization > item > item';
 
         var file = course.content.find((file) => {
             return file.name === 'imsmanifest.xml';
@@ -40,12 +60,29 @@ module.exports = (course, item, callback) => {
 
         var $ = file.dom;
         $(path).each((index, module) => {
-            var code = module.attribs.identifier;
-            var page = module;
+            var obj = {
+                'code': module.attribs.identifier,
+                'page': module.children[1].children[0].data,
+            }
+
+            // console.log(module.children[1].children);
+
+            xmlArray.push(obj);
         });
 
-    
-
         buildXMLArrayCallback(null);
+    }
+
+    function buildCanvasArray(buildCanvasArrayCallback) {
+        canvas.get(`/api/v1/courses/${course.info.canvasOU}/pages`, (getErr, pages) => {
+            if (getErr) {
+                buildCanvasArrayCallback(getErr);
+                return;
+            }
+
+            canvasPagesArray = [...pages];
+
+            buildCanvasArrayCallback(null);
+        });
     }
 }
