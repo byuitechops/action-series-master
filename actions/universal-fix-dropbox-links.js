3fa544639ea2842eadc621ef82bc1f1b52a2785b
Sam McGrath <mcg13004@byui.edu>
2018-04-09 09:04:34 -0600

    Fixed campus course checking

--- actions/universal-fix-dropbox-links.js
+++ actions/universal-fix-dropbox-links.js
@@ -25,7 +25,7 @@ module.exports = (course, item, callback) => {
      * beginProcess
      * 
      * This function acts as a driver for the program. It waterfalls 
-     * all of the functions.
+     * all of the functions and makes sure that they are completed.
     ******************************************************************/
     function beginProcess() {
         //ensure that the arrays are correctly populated before
