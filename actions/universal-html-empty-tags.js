3284cde45bdfb7afe664522330666cc7c03b18a7
Emma Fisher <35739518+emmafisher1720@users.noreply.github.com>
2018-04-09 09:02:16 -0600

    Update universal-html-empty-tags.js

--- actions/universal-html-empty-tags.js
+++ actions/universal-html-empty-tags.js
@@ -1,5 +1,7 @@
 const cheerio = require('cheerio');
 
+
+
 module.exports = (course, item, callback) => {
     /* Array of all tags to be searched */
     var arrayOfTags = ['p', 'span', 'div'];
@@ -14,13 +16,13 @@ module.exports = (course, item, callback) => {
 
         /* Filters to the html tags with no attributes nad no text */
         function toNoTextNoAttr(i, ele) {
-            return (Object.keys(ele.attribs).length === 0 && $(ele).text() === '');
+            return Object.keys(ele.attribs).length === 0 && $(ele).text() === '';
         }
 
         /* Create cheerio object and search them with FUNCTION and remove those*/
         arrayOfTags.forEach(function (element) {
             $(element).filter(toNoTextNoAttr).remove();
-        })
+        });
 
         /* Set the HTML to exclude the deleted parts */
         item.techops.setHTML(item, $.html());
@@ -33,8 +35,10 @@ module.exports = (course, item, callback) => {
         callback(null, course, item);
     }
 
+    var validPlatforms = ['Campus', 'Online', 'Pathway'];
+
     /* If HTML exists in the current item, call the action */
-    if (typeof item.techops.getHTML(item) != 'undefined' || item.techops.getHTML(item) != null) {
+    if ((typeof item.techops.getHTML(item) != 'undefined' || item.techops.getHTML(item) != null && item.techops.delete === false) && validPlatforms.includes(course.settings.platform)) {
         action();
     } else {
         callback(null, course, item);
