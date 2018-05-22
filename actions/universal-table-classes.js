69fc3a3140cb1a2b46e9bfc6f5b628d29d15c578
Seth Childers <sethchilders92@gmail.com>
2018-05-22 10:24:21 -0600

    Added universal-table-classes.js

---
+++ actions/universal-table-classes.js
@@ -0,0 +1,64 @@
+/*****************************************************
+ * Universal Table Classes
+ * Description: Give each table its needed CSS 
+ * classes (i.e. ic-Table, ic-Table--striped, etc)
+ ****************************************************/
+const cheerio = require('cheerio');
+
+module.exports = (course, item, callback) => {
+    try {
+        /* only add the platforms your grandchild should run in */
+        var validPlatforms = ['online', 'pathway'];
+        var validPlatform = validPlatforms.includes(course.settings.platform);
+
+        /* If the item is marked for deletion or isn't a valid platform type, do nothing */
+        if (item.techops.delete === true || validPlatform !== true) {
+            callback(null, course, item);
+            return;
+        }
+
+        /* Run if the test is passed */
+        function action() {
+            /* Get the html and each table element */
+            var $ = cheerio.load(item.techops.getHTML(item));
+            var tables = $('table').get();
+
+            /* For each table element, check its CSS classes. If it doesn't have the 'ic-Table' class, add it */
+            tables.forEach(table => {
+                var classes = $(table).attr('class');
+                var icTable = '';
+
+                /* If there are CSS classes, check if one is the 'ic-Table' class */
+                if (classes !== undefined) {
+                    classes = classes.split(' ');
+                    icTable = classes.find(currClass => /^ic-Table$/i.test(currClass));
+                }
+
+                /* If there are no CSS classes, or the table element doesn't have the ic-Table class, then add it */
+                if (icTable === undefined || classes === undefined) {
+                    $(table).addClass('ic-Table');
+
+                    /* Log it */
+                    item.techops.log(`${item.techops.type} - ic-Table Class Added`, {
+                        'Title': item.techops.getTitle(item),
+                        'ID': item.techops.getID(item),
+                    });
+                }
+            });
+
+            /* Set the HTML */
+            item.techops.setHTML(item, $.html());
+            callback(null, course, item);
+        }
+
+        /* If the item has html to check, run action() */
+        if (item.techops.getHTML(item) !== null) {
+            action();
+        } else {
+            callback(null, course, item);
+        }
+    } catch (e) {
+        course.error(new Error(e));
+        callback(null, course, item);
+    }
+};
